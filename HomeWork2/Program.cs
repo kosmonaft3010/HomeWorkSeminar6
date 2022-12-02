678ef99cdaad74409b12b24cdbc1317765ee45be
kosmonaft3010 <claud055876@gmail.com>
2022-12-02 13:51:14 +0300

    added_fixes

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -11,11 +11,15 @@ Console.Write("Введите k2: ");
 var k2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b2: ");
 var b2 = Convert.ToDouble(Console.ReadLine());
+if (k1 == k2)
+    Console.WriteLine("Прямые параллельны");
+else
+{
+    var x = -(b1 - b2) / (k1 - k2);
+    var y = k1 * x + b1;
 
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
+    x = Math.Round(x, 3);
+    y = Math.Round(y, 3);
 
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
-
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+    Console.WriteLine($"Пересечение в точке: ({x};{y})");
+}
